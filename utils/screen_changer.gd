58303e504eb790b4ceab7d597d32ba28dd214aff
Liam Hawks <68058931+hawkstein@users.noreply.github.com>
2025-09-27 09:45:49 +0100

    refactor screen changer to be Node2D
    Avoids having to pass in the callee as the autoload is part of the tree now

--- utils/screen_changer.gd
+++ utils/screen_changer.gd
@@ -2,20 +2,16 @@ extends Node
 
 @export var screens:Dictionary[String, PackedScene] = {}
 
-func _ready() -> void:
-	var root = get_tree().root
-	print(root)
-
-func change_to(current:Node, key:String) -> void:
+func change_to(key:String) -> void:
 	if not screens.has(key):
 		push_warning("Key does not exist: ", key)
 		return
-	if not current.is_inside_tree():
+	if not is_inside_tree():
 		push_error("Current node is not in the scene tree")
 		return
-	call_deferred("_deferred_goto_scene", current, key)
+	call_deferred("_deferred_change_to", key)
 
-func _deferred_change_to(current:Node, key:String) -> void:
+func _deferred_change_to(key:String) -> void:
 	await Fade.fade_out().finished
-	current.get_tree().change_scene_to_packed(screens[key])
+	get_tree().change_scene_to_packed(screens[key])
 	Fade.fade_in()
--- utils/ui/main_menu.gd
+++ utils/ui/main_menu.gd
@@ -4,4 +4,4 @@ func _ready() -> void:
 	DataStore.save_game_data()
 
 func _on_start_button_pressed() -> void:
-	ScreenChanger.change_to(self, "main")
+	ScreenChanger.change_to("main")
