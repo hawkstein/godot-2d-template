95fd10c79394ba8653901c204bdcbb16bf34bb6b
Liam Hawks <68058931+hawkstein@users.noreply.github.com>
2025-10-01 15:01:58 +0100

    add a little HUD to the top down

--- entities/topdown/top_down_player.gd
+++ entities/topdown/top_down_player.gd
@@ -1,14 +1,11 @@
 extends CharacterBody2D
 
-const MOVE_SPEED := 300
-const ACCELERATION : = 5.0
+const MAX_SPEED := 300
+const ACCELERATION := 5.0
+const DECELERATION := 5.0
 
 func _physics_process(delta: float) -> void:
-	var direction := Vector2.ZERO
-	direction.x = Input.get_axis("Left", "Right")
-	direction.y = Input.get_axis("Up", "Down")
-	direction = direction.normalized()
-	
-	velocity = lerp(velocity, direction * MOVE_SPEED, delta * ACCELERATION) 
+	var direction = Input.get_vector("Left", "Right", "Up", "Down")	
+	velocity = lerp(velocity, direction * MAX_SPEED, delta * ACCELERATION) 
 	
 	move_and_slide()
